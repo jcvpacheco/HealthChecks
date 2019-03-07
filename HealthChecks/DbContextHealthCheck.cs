255ff91e01984fb568161cc67290fdcbc88d6104
jcvpacheco <vasconcelos.pacheco@gmail.com>
2019-03-07 22:57:31 +0000

    Add project files.

---
+++ HealthChecks/DbContextHealthCheck.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Extensions.Diagnostics.HealthChecks;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace HealthChecks
+{
+    public class MyDbContextHealthCheck : IHealthCheck
+    {
+        private readonly MyDbContext _dbContext;
+
+
+        public MyDbContextHealthCheck(MyDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<HealthCheckResult> CheckHealthAsync(HealthCheckContext context,
+          CancellationToken cancellationToken = new CancellationToken())
+        {
+            return await _dbContext.Database.CanConnectAsync(cancellationToken)
+                    ? HealthCheckResult.Healthy()
+                    : HealthCheckResult.Unhealthy();
+        }
+    }
+}
---
+++ HealthChecks/Startup.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HealthChecks.UI.Client;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Newtonsoft.Json;
+
+namespace HealthChecks
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<MyDbContext>(options => options.UseInMemoryDatabase("DbContextHealthCheck"));
+
+            services.AddHealthChecks().AddCheck<MyDbContextHealthCheck>("DbContextHealthCheck");
+
+            services.AddHealthChecksUI();
+
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+
+
+            //Option 1
+            //app.UseHealthChecks("/health");
+
+            //Option 2
+            //app.UseHealthChecks("/health", new HealthCheckOptions
+            //{
+            //    ResponseWriter = async (context, report) =>
+            //    {
+            //        context.Response.ContentType = "application/json; charset=utf-8";
+            //        var bytes = Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(report));
+            //        await context.Response.Body.WriteAsync(bytes);
+            //    },
+
+            //    ResultStatusCodes =
+            //    {
+            //        [HealthStatus.Healthy] = StatusCodes.Status200OK,
+            //        [HealthStatus.Degraded] = StatusCodes.Status200OK,
+            //        [HealthStatus.Unhealthy] = StatusCodes.Status503ServiceUnavailable
+            //    }
+            //});
+
+            //Option 3
+            //Use / healthchecks-ui
+            app.UseHealthChecks("/health", new HealthCheckOptions()
+            {
+                Predicate = _ => true,
+                ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+            });
+
+            app.UseHealthChecksUI();
+
+            app.UseStaticFiles();
+            app.UseMvc();
+        }
+    }
+}
